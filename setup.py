72de785ae96cdf0a6b0dd6c522f060c55933baad
yokkora <freewave_3@hotmail.com>
2015-05-12 05:54:38 +0000

    mod version

--- setup.py
+++ setup.py
@@ -23,7 +23,7 @@ setup(
     # Versions should comply with PEP440.  For a discussion on single-sourcing
     # the version across setup.py and the project code, see
     # https://packaging.python.org/en/latest/single_source_version.html
-    version='1.0.0',
+    version='1.0.1',
 
     description='SudokuMaker which can make sudoku 4x4, 9x9, 16x16, , 100x100.',
     long_description=long_description,
