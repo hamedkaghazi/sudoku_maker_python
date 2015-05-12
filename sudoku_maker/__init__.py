1dff94c2edb12967ef3034e094a0390810857ede
yokkora <freewave_3@hotmail.com>
2015-05-12 04:16:48 +0000

    tweak

--- sudoku_maker/__init__.py
+++ sudoku_maker/__init__.py
@@ -13,4 +13,4 @@ def make(trgt_num=9):
     if trgt_num not in [4, 9, 16, 25, 36, 49, 64, 81, 100]:
         trgt_num = 9
     sudoku_maker = SudokuMaker(trgt_num)
-    sudoku_maker.make()
+    return sudoku_maker.make()
